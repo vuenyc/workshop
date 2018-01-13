5ce0adc2464b3526d9fa785bbeaf6e3bf3f83692
Zephraph <zephraph@gmail.com>
2018-01-12 21:48:45 -0500

    Resolved merge conflicts

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
-  <div>
-    <PlayerEntry/>
+  <div id="app">
+    <PlayerEntry :currentPlayer="currentPlayer" @playerJoined="addNewPlayer" v-if="step == 0"/>
+    <Lobby :currentPlayer="currentPlayer" :players="players" v-else-if="step == 1"/>
     <single-player-card />
   </div>
 </template>
 
 <script>
-import PlayerEntry from "./components/PlayerEntry";
+import PlayerEntry from './components/PlayerEntry';
+import Lobby from './components/Lobby';
 import SinglePlayerCard from "./components/SinglePlayerCard";
 
 export default {
+  data() {
+    return {
+      step: 0,
+      currentPlayer: {
+        name: '',
+        ready: false
+      },
+      players: []
+    }
+  },
+  methods: {
+    addNewPlayer(player) {
+      this.currentPlayer = player;
+      this.players.push(player);
+      this.step++;
+    }
+  },
   components: {
     PlayerEntry,
+    Lobby
     SinglePlayerCard
   }
 };
 </script>
+
+<style>
+  html, body, #app {
+    height: 100%;
+    padding: 0;
+    margin: 0;
+  }
+</style>
+
