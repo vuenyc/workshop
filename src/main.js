c4af2e7f21e1d471a82ce15276107734b30f3fc3
Zephraph <zephraph@gmail.com>
2018-01-16 19:45:07 -0500

    Adding element ui

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import locale from 'element-ui/lib/locale/lang/en'
+
+Vue.use(ElementUI, { locale })
 
 new Vue({
   el: '#app',
