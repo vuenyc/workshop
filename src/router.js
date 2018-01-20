550da1205f2f2d6f507dfbf28dc6bafd48878c49
ringo kam <ringo_kam@hotmail.com>
2018-01-19 23:45:31 -0500

    fixed routing

--- src/router.js
+++ src/router.js
@@ -1,12 +1,11 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-import PlayerEntry from './components/PlayerEntry.vue';
-import Lobby from './components/Lobby.vue';
-import PendingState from './components/PendingState.vue';
-import VotingState from './components/VotingState.vue';
-import EndGame from './components/EndGame.vue';
-import GameSession from './components/GameSession.vue';
+import PlayerEntry from './components/PlayerEntry.vue'
+import Lobby from './components/Lobby.vue'
+import PendingState from './components/PendingState.vue'
+import VotingState from './components/VotingState.vue'
+import EndGame from './components/EndGame.vue'
 
 Vue.use(VueRouter);
 
@@ -16,39 +15,28 @@ export const routes = [
     name: 'PlayerEntry',
     component: PlayerEntry,
     props: true
-  },
-  {
+  }, {
     path: '/lobby',
     name: 'Lobby',
     component: Lobby,
     props: true
-  },
-  {
-    path: '/game',
-    name: 'Game',
-    component: GameSession,
-    children: [
-      {
-        path: '/wait',
-        name: 'PendingState',
-        component: PendingState,
-        props: true
-      },
-      {
-        path: '/vote',
-        name: 'VotingState',
-        component: VotingState,
-        props: true
-      }
-    ]
-  },
-  {
+  }, {
+    path: '/game/wait',
+    name: 'PendingState',
+    component: PendingState,
+    props: true
+  }, {
+    path: '/game/vote',
+    name: 'VotingState',
+    component: VotingState,
+    props: true
+  }, {
     path: '/game/over',
     name: 'EndGame',
     component: EndGame,
     props: true
   }
-];
+]
 
 export default new VueRouter({
   routes
