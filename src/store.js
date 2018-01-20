5fd3957a3d17ddbb518a1ea0f9825d78af5636c0
ringo kam <ringo_kam@hotmail.com>
2018-01-20 00:03:33 -0500

    added routing logic into game

--- src/store.js
+++ src/store.js
@@ -1,44 +1,52 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import router from './router'
+import Vue from "vue";
+import Vuex from "vuex";
+import router from "./router";
 
-import roles from './api/role'
+import roles from "./api/role";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    currentPlayer: {
-      name: 'ENTER YOUR NAME HERE',
-    },
-    allPlayers:[
-    ]
+    playerName: "ENTER YOUR NAME HERE",
+    allPlayers: []
   },
   mutations: {
     addPlayer(state, player) {
       state.allPlayers.push(player);
-    }, 
+    },
     saveCurrentPlayer(state, playerName) {
-      state.currentPlayer.name = playerName;
-    }, 
+      state.playerName = playerName;
+    },
     setRoles(state, roles) {
       state.allPlayers = state.allPlayers.map(player => {
-        player.role = roles.pop(); 
+        player.role = roles.pop();
         return player;
-      })
-    } 
+      });
+    }
   },
   actions: {
-    enterLobby({commit}, playerName) {
+    enterLobby({ commit }, playerName) {
       commit("addPlayer", {
-        name: playerName, 
-        imgUrl: "https://www.ald.softbankrobotics.com/sites/aldebaran/files/images/en_savoir_plus_sur_nao_2.png"
+        name: playerName,
+        imgUrl:
+          "https://www.ald.softbankrobotics.com/sites/aldebaran/files/images/en_savoir_plus_sur_nao_2.png"
       });
       commit("saveCurrentPlayer", playerName);
     },
-    startGame({state, commit}) {
-      commit("setRoles", roles)
-      router.push('game');
+    startGame({ state, commit, getters }) {
+      commit("setRoles", roles);
+      debugger;
+      router.push(
+        `game/${getters.currentPlayer.role === "Werewolf" ? "vote" : "wait"}`
+      );
+    }
+  },
+  getters: {
+    currentPlayer: (state) => {
+      return state.allPlayers.filter(
+        player => player.name === state.playerName
+      )[0];
     }
   }
-})
+});
