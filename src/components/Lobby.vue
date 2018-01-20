ce9fcafccfb88bcfead6e7b02649ee33509eb899
tessaSAC <tessaSAC@users.noreply.github.com>
2018-01-19 22:17:35 -0500

    add styling to lobby

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -1,38 +1,94 @@
 <template>
   <div class="Lobby">
-    <!-- <PlayerList/> -->
-    <h1>Waiting for {{ playersLeft }} players to start</h1>
-    <button :disabled="thisPlayer.ready" @click="ready">Start</button>
-    <ul>
-      <li v-for="player in players" :key="player.name">
-        {{ player.name }} -- Ready? {{ player.ready }}
-      </li>
-    </ul>
+
+    <div class="room-info">
+      <h1>Game Lobby</h1>
+      <p>{{ playersLeft }} more players needed to begin game</p>
+    </div>
+
+    <div class="waiting-players">
+      <SinglePlayerCard v-for="(player, idx) in players" :key="idx" :player="player" />
+    </div>
+
+    <button :disabled="playersLeft" @click="ready">start game</button>
+
   </div>
 </template>
 
 <script>
+import SinglePlayerCard from './SinglePlayerCard'
+
 export default {
-  props: ['players', 'thisPlayer'],
+  components: {
+    SinglePlayerCard,
+  },
+  // props: ['players', 'thisPlayer'],
   data() {
     return {
-      playersLeft: 7
+      playersLeft: 2,
+      players: [
+        {
+            imgUrl: 'https://i.pinimg.com/236x/2b/8d/29/2b8d29c19ca209b35b14e91ef60e9100.jpg',
+            name: 'pringo',
+            role: 'popping',
+            description: 'once you pop you can\'t stop'
+        }, {
+            imgUrl: 'https://www.ald.softbankrobotics.com/sites/aldebaran/files/images/en_savoir_plus_sur_nao_2.png',
+            name: 'justin',
+            role: 'popping',
+            description: 'once you pop you can\'t stop'
+        }, {
+            imgUrl: 'https://i.pinimg.com/236x/2b/8d/29/2b8d29c19ca209b35b14e91ef60e9100.jpg',
+            name: 'pringo',
+            role: 'popping',
+            description: 'once you pop you can\'t stop'
+        }, {
+            imgUrl: 'https://www.ald.softbankrobotics.com/sites/aldebaran/files/images/en_savoir_plus_sur_nao_2.png',
+            name: 'justin',
+            role: 'popping',
+            description: 'once you pop you can\'t stop'
+        }, {
+            imgUrl: 'https://www.ald.softbankrobotics.com/sites/aldebaran/files/images/en_savoir_plus_sur_nao_2.png',
+            name: 'justin',
+            role: 'popping',
+            description: 'once you pop you can\'t stop'
+        },
+      ]
     }
   },
   methods: {
-    ready() {
-      this.playersLeft--;
-      this.thisPlayer.ready = true;
-      //TODO: go to next stage by client side, i think we need service side logic here. 
-      setTimeout(() => {
-        this.$emit('nextStep');
-      }, 3000);
-    }
   }
 }
 </script>
 
 <style scoped>
+  .SinglePlayerCard {
+    flex-basis: 100%;
+    padding: 3vw;
+  }
+  .room-info {
+    margin-bottom: 5vh;
+  }
 
+  @media screen and (min-width: 800px) {
+    .waiting-players, button {
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+    }
+    h1, p {
+      flex: 1;
+      text-align: center;
+    }
+    .SinglePlayerCard {
+      flex: 2.5;
+    }
+    button {
+      margin: 0 auto;
+      padding: 2vh 5vw;
+      text-align: center;
+      font-size: 2rem;
+    }
+  }
 </style>
 
--- src/components/SinglePlayerCard.vue
+++ src/components/SinglePlayerCard.vue
@@ -10,7 +10,7 @@
 <script>
 export default {
     // player: {,
-    //     imgUrl: 'https://i.pinimg.com/236x/2b/8d/29/2b8d29c19ca209b35b14e91ef60e9100.jpg'
+    //     imgUrl: 'https://i.pinimg.com/236x/2b/8d/29/2b8d29c19ca209b35b14e91ef60e9100.jpg',
     //     name: 'pringo',
     //     role: 'popping',
     //     description: 'once you pop you can\'t stop'
@@ -34,5 +34,7 @@ export default {
 
     img {
         border-radius: 50%;
+        height: 10vw;
+        width: 10vw;
     }
 </style>
